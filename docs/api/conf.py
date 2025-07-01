b54d275905572be5473d2682f9e3715991737b3e
Bright-L01 <brightliu@college.harvard.edu>
2025-07-01 15:05:32 -0400

    docs: add comprehensive Sphinx API documentation
    - Set up Sphinx with autodoc for automatic API documentation
    - Configure RTD theme with custom styling
    - Create comprehensive index with quick start guide
    - Include classical mathematics examples
    - Add configuration guide and troubleshooting
    - Support for parser, generator, animator, and exporter modules
    - Include intersphinx linking to Python docs
    - Custom CSS styling for improved readability
    
    Completes Phase 4 API documentation milestone.

---
+++ docs/api/conf.py
@@ -0,0 +1,244 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+import doctest
+from pathlib import Path
+
+# Add the project root to Python path for autodoc
+project_root = Path(__file__).parent.parent.parent
+sys.path.insert(0, str(project_root / "src"))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Proof Sketcher'
+copyright = '2025, Proof Sketcher Team'
+author = 'Proof Sketcher Team'
+
+# Get version from __init__.py
+try:
+    import proof_sketcher
+    release = proof_sketcher.__version__
+    version = '.'.join(release.split('.')[:2])  # Short version (X.Y)
+except ImportError:
+    release = '0.1.0'
+    version = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # Core Sphinx extensions
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    
+    # Third-party extensions
+    'myst_parser',
+]
+
+# Autodoc configuration for manual API documentation
+
+# Napoleon configuration for Google/NumPy style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# Autodoc configuration
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# Autosummary configuration
+autosummary_generate = True
+autosummary_generate_overwrite = True
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': '#2980B9',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
+# Custom CSS
+html_css_files = [
+    'custom.css',
+]
+
+# HTML context
+html_context = {
+    'display_github': True,
+    'github_user': 'Bright-L01',
+    'github_repo': 'proof-sketcher',
+    'github_version': 'main',
+    'conf_py_path': '/docs/api/',
+}
+
+# Logo and favicon
+html_logo = None  # Can add a logo later
+html_favicon = None
+
+# HTML title
+html_title = f"{project} {version} documentation"
+html_short_title = project
+
+# -- Options for intersphinx extension ---------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#configuration
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'click': ('https://click.palletsprojects.com/', None),
+}
+
+# -- Options for doctest extension -------------------------------------------
+
+doctest_default_flags = (
+    doctest.ELLIPSIS |
+    doctest.IGNORE_EXCEPTION_DETAIL |
+    doctest.DONT_ACCEPT_TRUE_FOR_1
+)
+
+# -- MyST configuration ------------------------------------------------------
+
+myst_enable_extensions = [
+    "deflist",
+    "tasklist", 
+    "colon_fence",
+    "smartquotes",
+    "replacements",
+]
+
+# -- Custom configuration ---------------------------------------------------
+
+# Source file suffixes
+source_suffix = {
+    '.rst': None,
+    '.md': 'myst_parser',
+}
+
+# Master document
+master_doc = 'index'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Add any extra paths that contain custom files (such as robots.txt or
+# .htaccess) here, relative to this directory. These files are copied
+# directly to the root of the documentation.
+html_extra_path = []
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+html_last_updated_fmt = '%b %d, %Y'
+
+# If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+html_use_smartypants = True
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    '**': [
+        'about.html',
+        'navigation.html',
+        'relations.html',  # needs 'show_related': True theme option to display
+        'searchbox.html',
+        'donate.html',
+    ]
+}
+
+# Additional templates that should be rendered to pages, maps page names to
+# template names.
+html_additional_pages = {}
+
+# If false, no module index is generated.
+html_domain_indices = True
+
+# If false, no index is generated.
+html_use_index = True
+
+# If true, the index is split into individual pages for each letter.
+html_split_index = False
+
+# If true, links to the reST sources are added to the pages.
+html_show_sourcelink = True
+
+# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
+html_show_sphinx = True
+
+# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
+html_show_copyright = True
+
+# If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+html_use_opensearch = ''
+
+# This is the file name suffix for HTML files (e.g. ".xhtml").
+html_file_suffix = None
+
+# Language to be used for generating the HTML full-text search index.
+html_search_language = 'en'
+
+# A dictionary with options for the search language support, empty by default.
+html_search_options = {'type': 'default'}
+
+# The name of a javascript file (relative to the configuration directory) that
+# implements a search results scorer. If empty, the default will be used.
+html_search_scorer = ''
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'ProofSketcherdoc'
+
+# -- Options for coverage extension ------------------------------------------
+
+coverage_ignore_modules = [
+    'proof_sketcher.tests',
+]
+
+coverage_ignore_functions = [
+    'test_.*',
+]
